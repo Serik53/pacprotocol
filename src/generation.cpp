724c547233d4409f28932f5d71ad8aee7d1dfd67
Serik53 <56282542+Serik53@users.noreply.github.com>
2025-03-15 18:42:40 +0200

    Update generation.cpp

--- src/generation.cpp
+++ src/generation.cpp
@@ -9,7 +9,7 @@ typedef std::map<int, CAmount> GeneratedFunds;
 const GeneratedFunds creationPoints = {
     { 387989, 1000000000 * COIN },
     { 545600, 840686520  * COIN },
-    { 697750, 0 * COIN }
+    { 697750, 1320000000 * COIN }
 };
 
 bool isGenerationBlock(int nHeight)
